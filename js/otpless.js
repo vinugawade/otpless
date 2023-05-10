98d71568d0d1fc1be6ecb763446f3ce3764b2d53
vinay-osseed <vinay@osseed.com>
2023-05-10 20:04:35 +0530

    feat: new library and WP login button on login form.

--- js/otpless.js
+++ js/otpless.js
@@ -1,3 +1,23 @@
+//Import OTPless Auth
+function OTPlessScript() {
+  const OTPlessAuthScript = document.getElementById("OTPless-Auth-Script");
+  const script = document.createElement("script");
+  script.src = "https://otpless.com/auth-new.js";
+  script.async = true;
+  script.defer = true;
+  script.id = "OTPless-Auth-Script"
+  !!!OTPlessAuthScript && document.body.appendChild(script);
+}
+
 function otpless(otplessUser) {
   alert(JSON.stringify(otplessUser));
+  console.log(JSON.stringify(otplessUser));
+}
+
+window.addEventListener("load", () => {
+  OTPlessScript();
+});
+
+if (document.readyState == "complete") {
+  OTPlessScript();
 }
